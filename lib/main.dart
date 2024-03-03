42a29f89da3d3b3d19ff0970f0b559374b6e6b0b
sandesh <gsandace@gmail.com>
2024-03-03 22:53:27 +0545

    unnessary import removed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:portfoliio/landing_page.dart';
+import 'package:portfoliio/ui/pages/landing_page.dart';
 
 void main() {
   runApp(const MyApp());
