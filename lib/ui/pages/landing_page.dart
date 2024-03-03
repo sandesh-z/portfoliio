8f55b0e382e3e76b71221203b21fcd77cd6e550d
sandesh <gsandace@gmail.com>
2024-03-03 17:58:49 +0545

    minor design changes

--- lib/ui/pages/landing_page.dart
+++ lib/ui/pages/landing_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-import 'package:portfoliio/ui/card.dart';
+import 'package:portfoliio/ui/widgets/app_card.dart';
 
 class LandingPage extends StatefulWidget {
   const LandingPage({super.key});
@@ -9,13 +8,22 @@ class LandingPage extends StatefulWidget {
   State<LandingPage> createState() => _LandingPageState();
 }
 
+//demo: https://www.flo-design.eu/
 class _LandingPageState extends State<LandingPage> {
   @override
   Widget build(BuildContext context) {
     TextStyle appTextStyle = const TextStyle(
-        color: Colors.white, fontWeight: FontWeight.w500, fontSize: 14);
+        color: Colors.white, fontWeight: FontWeight.w500, fontSize: 18);
+    var titleGap = 20.0;
     return Scaffold(
       appBar: AppBar(
+        leading: const Padding(
+            padding: EdgeInsets.only(left: 40.0),
+            child: Icon(
+              Icons.person_4,
+              size: 28,
+            )),
+        toolbarHeight: 79,
         title: Row(
           mainAxisAlignment: MainAxisAlignment.end,
           children: [
@@ -23,19 +31,20 @@ class _LandingPageState extends State<LandingPage> {
               "About",
               style: appTextStyle,
             ),
-            const SizedBox(width: 10),
+            SizedBox(width: titleGap),
             Text(
-              "Work",
+              "Projects",
               style: appTextStyle,
             ),
-            const SizedBox(width: 10),
+            SizedBox(width: titleGap),
             Text(
               'Contact',
               style: appTextStyle,
-            )
+            ),
+            SizedBox(width: titleGap * 2),
           ],
         ),
-        backgroundColor: Colors.black,
+        backgroundColor: Colors.grey,
       ),
       body: Column(children: [
         Expanded(
